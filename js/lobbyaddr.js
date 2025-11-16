53c18e84cdd2dcc1b37182aeb61e1f79facb06ce
H4TIUX <162725013+H4TIUX@users.noreply.github.com>
2025-11-16 15:25:01 +0100

    1

---
+++ js/lobbyaddr.js
@@ -0,0 +1,12 @@
+var lobbies = [
+    {
+        name: "CN",
+        id: "cn",
+        address: "http://gb55841a5.xy.proaa.top/pubg/",
+    },
+    {
+        name: "EU - Main",
+        id: "eu",
+        address: "https://lobby.ogbattlegrounds.com/",
+    }
+]
---
+++ js/serverList.js
@@ -0,0 +1,32 @@
+var serverList = [
+
+    {
+
+        name: "AS",
+
+        description: "Asia Server",
+
+        ip: "202.189.7.13:64749" 
+
+    },
+
+    {
+
+        name: "EU",
+
+        description: "European Server",
+
+        ip: "185.254.158.46:7777" 
+
+    },
+
+    {
+
+        name: "RU",
+
+        description: "Russian Server",
+
+        ip: "81.177.135.63:8888" 
+
+    }
+]
